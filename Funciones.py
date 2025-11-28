b4aaf0b621999223361476719cd5f6734dd87efd
Adolpho <adolphovt12@gmail.com>
2025-11-27 21:10:45 -0600

    Funciones en python

---
+++ Funciones.py
@@ -0,0 +1,10 @@
+#una funcion es una o varias lineas de cogico que tienen una tarea o cometido
+
+print("Bienvenido al programa")
+num1 = (int(input("introduce el primer número: ")))
+num2 =(int(input("introduce el segundo número: ")))
+
+def suma():
+    resultado= num1+num2
+    return resultado
+print(suma())

8150c62cf4aaa88210a2d1ec722bd7b1eb0fc9b0
adocoop <adocoop@localhost.localdomain>
2025-12-01 20:23:33 -0600

    Las listas

---
+++ Listas.py
@@ -0,0 +1,7 @@
+miLista=["Maria", "kasandra", "Daniela", "Melissa Perez"]
+
+#Para acceder a un indice en especifico ponemos el numero entre corchetes
+print(miLista[3])
+
+
+#cuando introducimos un indice nagativo dara la vuelta a la lista

c11e8d85e64d97d962f147fb9664b0275a4d7b65
adocoop <adocoop@localhost.localdomain>
2025-12-02 20:11:53 -0600

    Listas repaso

--- Listas.py
+++ Listas.py
@@ -1,7 +1,18 @@
 miLista=["Maria", "kasandra", "Daniela", "Melissa Perez"]
+#append funciona para agregar algo a la lista 
+
+miLista.append("Sandra")
+#para usar insert debemos indicar el indice donde se agregara el otro elemento
+miLista.insert(2,"Andrea mamita")
+#Extend es una concatenacion agregamos mas elementos
+
+miLista.extend(["Mayra", "Pamela", "Fany"])
 
 #Para acceder a un indice en especifico ponemos el numero entre corchetes
-print(miLista[3])
+#para comprobar si un elemento se encuentra
+#print("Pepe" in miLista)
+
+print(miLista.index("Andrea mamita"))
 
 
 #cuando introducimos un indice nagativo dara la vuelta a la lista

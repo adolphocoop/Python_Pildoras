a9ba0659ba4722a8a51d2dbf2317179997b07b1e
Adolpho <adolphovt12@gmail.com>
2025-12-18 20:54:46 -0600

    Aprendiedo del if

---
+++ Else_if.py
@@ -0,0 +1,14 @@
+#Imprimimos el siguiente mensaje
+
+#preguntamos la edad al usuario por teclado
+edad_usuario=int(input("Introduce la edad por favor: "))
+
+#Evaluar si la edad es mayor a 18
+
+if edad_usuario<18:
+    print("No puedes pasar")
+
+else:
+    print("Tienes acceso")
+
+print("El programa ha finalizado")

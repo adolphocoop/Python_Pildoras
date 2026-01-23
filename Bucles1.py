6c3b2c9ee1a59359bb77bc3da2bdc1a85b4ff300
adocoop <adocoop@localhost.localdomain>
2026-01-22 20:27:54 -0600

    Aprendiendo del bucle for

---
+++ Bucles1.py
@@ -0,0 +1,16 @@
+"""
+Bucles determinados son aquellos que antes de su ejecucion ya sabemos cuantas veces va a repetir el codigo
+Los bucles indeterminados no se sabe a priori cuantas veces se vaya a ejecutar el codigo
+"""
+
+#Bucles determinados los que se sabe cuantes veces se va aejecutar el codigo
+
+#Sintaxis for variable "in" elemento a recorrer
+
+"""for i in [1, 2, 3, 4, 5]:
+    print("Es la posicion " + str(i))"""
+
+
+#La variable "i" puede cambiar de nombre y al igual recorrera
+for i in ["Primavera", "Verano", "Otonio", "Invierno"]:
+    print(i)

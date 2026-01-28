bf00279adda8a0f2c3dec0ffe9b40e63f14137fa
adocoop <adocoop@localhost.localdomain>
2026-01-27 20:51:29 -0600

    Repaso del range

--- For.py
+++ For.py
@@ -1,5 +1,12 @@
 
 #En python 3 es un tipo y en python 2 es una funcion
-for i in range(5):
-    print("Hola")
+
+#el tipo range nos devuelve un tipo array
+
+
+for i in range(5, 10):#hara el conteo del 5 al 9
+    #para concatenar valores de texto y de variables
+    # "F" es una notacion especial, lo que nos permite jugar con formatos distintos
+
+    print(f"Valor de la variable  {i}")
 
